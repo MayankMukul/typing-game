7e67f768516f254250de15b484db23ce3e473963
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-11 16:48:24 +0530

    standby

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-var quotebox = document.querySelector(".quote-box") ;
+var quotebox = document.querySelector(".quote-box");
 var message = document.querySelector(".message");
 var inputbox = document.querySelector("#input-box");
 var btn = document.querySelector("#btn-start");
 var output = document.querySelector(".output");
 var quotes = ['This is a sample quote1','this is sample quote 2.'];
+var word = [];
+var wordindex=0;
+var starttime = Date.now();
 
-var randomQuote = function(){
+function randomQuote () {
     var index= Math.floor(Math.random() *quotes.length);
-    console.log(index);
-    return quotes[index];
+    word = quotes[index].split(' ');
+    quotebox.innerText = quotes[index];
 }
-randomQuote();
-console.log(randomQuote());
+
+function messagedisplay(){
+    
+        message.style="display:none";
+  
+}
+
+function process() {
+    messagedisplay();
+    randomQuote();
+    
+}
+
+function typing(){
+    message.innerText="user is typing.."
+}
+
+btn.addEventListener("click",process);
+inputbox.addEventListener("OnChange",typing);
