28ea9798582c2ff53ab5a7d67b1d8d5e449735d8
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-09-07 17:43:10 +0530

    basic typing game done

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ var message = document.querySelector(".message");
 var inputbox = document.querySelector("#input-box");
 var btn = document.querySelector("#btn-start");
 var output = document.querySelector(".output");
-var quotes = ['This is a sample quote1','this is sample quote 2.'];
+var quotes = [
+    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+    'There is nothing more deceptive than an obvious fact.',
+    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
+    'I never make exceptions. An exception disproves the rule.',
+    'What one man can invent another can discover.',
+    'Nothing clears up a case so much as stating it to another person.',
+    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+];
 var word = [];
 var wordindex=0;
 var starttime = Date.now();
@@ -11,24 +19,72 @@ var starttime = Date.now();
 function randomQuote () {
     var index= Math.floor(Math.random() *quotes.length);
     word = quotes[index].split(' ');
-    quotebox.innerText = quotes[index];
+    
+    let spanword = word.map((word)=>{
+        return `<span class="word">${word} </span>`;
+    })
+    quotebox.innerHTML = spanword.join('');
+
+    quotebox.childNodes[0].className = 'highlight';
+
+    message.innerText=" ";
+
+    inputbox.value = "";
+    
+
+
 }
 
-function messagedisplay(){
-    
-        message.style="display:none";
-  
-}
 
 function process() {
-    messagedisplay();
+    word = [];
+    wordindex=0;
+    starttime = new Date().getTime();
     randomQuote();
+    inputbox.removeAttribute("disabled",null);
+    inputbox.focus();
     
 }
 
 function typing(){
-    message.innerText="user is typing.."
+     
+    message.innerText="user is typing..";
+
+    let currentword = word[wordindex];
+    let inputvalue = inputbox.value;
+
+    if(inputvalue === currentword && wordindex === word.length-1){
+        let time = new  Date().getTime() - starttime;
+        message.innerHTML = `Time elapsed : ${(time)/1000}`;
+        inputbox.setAttribute("disabled",null);
+
+
+    }else if(inputvalue.endsWith(' ') && inputvalue.trim() === currentword){
+        
+
+        wordindex++;
+
+        for(let wordelement of quotebox.childNodes){
+            wordelement.className = "";
+        }
+
+        quotebox.childNodes[wordindex].className = "highlight";
+
+        inputbox.value = "";
+    }else if (currentword.startsWith(inputvalue)){
+        inputbox.className = "green";
+    } else {
+        inputbox.className = "error";
+    }
+    // console.log(inputvalue.value)
+    // console.log(inputvalue.trim())
+
+    // console.log(wordindex,word.length-1)
+
+    // if (inputbox.value == word){
+    //     console.log("match")
+    // } else console.log("did not match")
 }
 
 btn.addEventListener("click",process);
-inputbox.addEventListener("OnChange",typing);
+inputbox.addEventListener("input",typing);
